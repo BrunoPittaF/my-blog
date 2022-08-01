6f7bd7a679b6585b717943bbc82b2d23bd5163b1
Caio <caiobrunopittaf@gmail.com>
2022-08-01 17:28:50 -0300

    fix: correcao de links no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,45 +1,50 @@
 import styles from './styles.module.scss';
 import Image from 'next/image';
 import ActiveLink from '../ActiveLink';
+import Link from 'next/link';
 
 export function Header() {
   return (
     <>
       <header className={styles.headerContainer}>
         <div className={styles.logo}>
-          <Image
-            className="desktopImage"
-            layout="fill"
-            objectFit="cover"
-            src="/images/logo.svg"
-            alt="Dono do website"
-            title="Dono do website"
-          />
-          <Image
-            className="mobileImage"
-            layout="fill"
-            objectFit="cover"
-            src="/images/logo-mobile.svg"
-            alt="Dono do website"
-            title="Dono do website"
-          />
+          <Link href="/">
+            <a>
+              <Image
+                className="desktopImage"
+                layout="fill"
+                objectFit="cover"
+                src="/images/logo.svg"
+                alt="Dono do website"
+                title="Dono do website"
+              />
+              <Image
+                className="mobileImage"
+                layout="fill"
+                objectFit="cover"
+                src="/images/logo-mobile.svg"
+                alt="Dono do website"
+                title="Dono do website"
+              />
+            </a>
+          </Link>
         </div>
 
         <nav>
           <ul>
             <li>
               <ActiveLink activeClassName={styles.active} href="/">
-                <span>Home</span>
+                <a>Home</a>
               </ActiveLink>
             </li>
             <li>
               <ActiveLink activeClassName={styles.active} href="/experience">
-                <span>Experiência</span>
+                <a>Experiência</a>
               </ActiveLink>
             </li>
             <li>
               <ActiveLink activeClassName={styles.active} href="/articles">
-                <span>Artigos</span>
+                <a>Artigos</a>
               </ActiveLink>
             </li>
           </ul>
